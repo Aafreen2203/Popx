fd6a92a8dcd6a02844c569f03a976110100b616c
Aafreen2203 <aafreen22k@gmail.com>
2025-03-24 14:32:38 +0530

    LandingPage

---
+++ src/LandingPage/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="landing-container">
+      <div className="landing-content">
+        <h1>Welcome to PopX</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <button className="create-account">Create Account</button>
+        <button className="login-button">Already Registered? Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
