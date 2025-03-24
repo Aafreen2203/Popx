9cad604b30f52acf62a279dc46c3b0701a37595b
Aafreen2203 <aafreen22k@gmail.com>
2025-03-24 21:52:03 +0530

    profile styling done

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import "./Profile.css";
-import profile from "../assets/profile.png"
+import profile from "../assets/profile.png";
+import cameraimg from "../assets/cameraimg.svg"; 
 
 const Profile = () => {
   return (
     <div className="profile-container">
-      <div className="account-settings">
-        <img src={profile} alt="Profile" />
-        <div>
-          <h2>Marry Doe</h2>
-          <p>Marry@gmail.com</p>
+      <div className="profile-titlecont">
+        <h3 className="profile-title">Account Settings</h3>
+      </div>
+
+      <div className="profile-card">
+        <div className="profile-image-container">
+          <img src={profile} alt="Profile" className="profile-image" />
+          <img src={cameraimg} alt="Camera" className="camera-icon" />
+        </div>
+
+        <div className="profile-info">
+          <h2 className="profile-name">Marry Doe</h2>
+          <p className="profile-email">Marry@gmail.com</p>
         </div>
       </div>
-      <p>
-        Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Diam Nonummy 
-        Ermod Tempor Invidunt Ut Labore Et Dolore Magna Aliquam Erat, Sed Diam
+
+      <p className="profile-description">
+        Lorem Ipsum Dolor Sit Amet, Consetetur Sadipscing Elitr, Sed Diam 
+        Nonumy Eirmod Tempor Invidunt Ut Labore Et Dolore Magna Aliquam Erat, Sed Diam
       </p>
     </div>
   );
